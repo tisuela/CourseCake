c6967eb8458ddffce16de432be10df37321f8a28
nananananate <=>
2020-07-13 19:29:38 -0700

    added support Course (object) -> Courses (sqlite table) support

--- coursecake/flaskapp/__init__.py
+++ coursecake/flaskapp/__init__.py
@@ -53,14 +53,39 @@ class Test(db.Model):
 
 
 
+
 class Courses(db.Model):
     code = db.Column(db.String(20), primary_key=True, nullable = False)
     name = db.Column(db.String(50), nullable = False)
+    title = db.Column(db.String(100), nullable = False)
+    instructor = db.Column(db.String(50), nullable = False)
+    time = db.Column(db.String(100), nullable = False)
+    location = db.Column(db.String(50), nullable = False)
+    status = db.Column(db.String(20), nullable = False)
+
+    units = db.Column(db.Integer, nullable = False)
+
+    type = db.Column(db.String(20), nullable = True)
+
 
 
     def __init__(self, course):
+        '''
+        Courses uses a course objects
+        See ../scraper/course.py
+        '''
         self.code = course.code
         self.name = course.name
+        self.title = course.title
+        self.instructor = course.instructor
+        self.time = course.time
+        self.location = course.location
+        self.status = course.status
+
+        self.units = course.units
+
+        self.type = course.type
+
 
     def __repr__(self):
-        return f"{self.code} {self.name}"
+        return f"{self.code} | {self.name} | {self.instructor} | {self.units} | {self.status} \n"
