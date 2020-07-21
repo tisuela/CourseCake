853e9263e35f4e03dbd0ffcf97b448454c31d310
nananananate <piewalker442@gmail.com>
2020-07-21 12:22:39 -0700

    added environ vars

--- coursecake/config.py
+++ coursecake/config.py
@@ -1,7 +1,15 @@
 '''
-Modify this file for deployment
+Modify this file or your environmental vars for production deployment
 '''
+import os
+
 
 class Config:
     SECRET_KEY = "dev"
     SQLALCHEMY_DATABASE_URI = "sqlite:///../db.sqlite"
+
+    if (os.environ.get("SECRET_KEY") != None):
+        SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    if (os.environ.get("SQLALCHEMY_DATABASE_URI ") != None):
+        SQLALCHEMY_DATABASE_URI  = os.environ.get("SQLALCHEMY_DATABASE_URI ")
