df0f71bf3ecadeb949950f96160661266c2f417b
nananananate <piewalker442@gmail.com>
2020-07-22 16:51:48 -0700

    added home page (readme.md)

---
+++ coursecake/flaskapp/home/routes.py
@@ -0,0 +1,21 @@
+from flask import Blueprint
+import markdown
+import markdown.extensions.fenced_code
+
+from ..limiter import limiter
+
+
+home_blueprint = Blueprint("home_blueprint", __name__)
+
+
+@home_blueprint.route("/")
+@home_blueprint.route("/index")
+@home_blueprint.route("/readme")
+def homePage():
+    readMe = open("README.md", "r")
+    mdTemplateStr = markdown.markdown(
+        readMe.read(),
+        extensions = ["fenced_code"]
+    )
+
+    return mdTemplateStr
