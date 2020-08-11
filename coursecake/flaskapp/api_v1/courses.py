2fb3858a1bfc1f852016729871deffd8e22ef3a6
nananananate <piewalker442@gmail.com>
2020-08-11 11:17:47 -0700

    revised query string

--- coursecake/flaskapp/api_v1/courses.py
+++ coursecake/flaskapp/api_v1/courses.py
@@ -2,7 +2,7 @@ from flask import make_response,jsonify,request,Blueprint,current_app
 from flask_restx import Namespace, Resource, fields
 
 from ..limiter import limiter
-from ..queries import handleCourseSearch,handleUciLiveSearch,\
+from ..queries import CourseSearch,handleUciLiveSearch,\
                         queryAllCourses,packageResults
 
 api =  Namespace("courses", description= "Search for courses")
@@ -26,13 +26,18 @@ class AllCourses(Resource):
 class Search(Resource):
     @api.response(200, "Success")
     @api.doc(params = {
-            "code": {"in": "query", "description": "Unique course code"},
-            "department": {"in": "query", "description": "Unique department code"}
+            "code": {
+                "in": "query",
+                "description": "Unique course code"},
+            "department": {
+                "in": "query",
+                "description": "Unique department code"}
     })
     def get(self, university: str):
         current_app.logger.info("course search requested")
         args = request.args
-        results = handleCourseSearch(university.upper(), args)
+        search = CourseSearch(university.upper(), args)
+        results = search.search()
         courseData = packageResults(results)
 
         headers = {"Content-Type": "application/json"}
--- coursecake/flaskapp/queries.py
+++ coursecake/flaskapp/queries.py
@@ -1,6 +1,8 @@
 '''
 This module handles all queries made to the database
 '''
+import re
+
 from .models import University, Courses, CoursesSchema
 from ..scrapers.course_scraper import CourseScraper
 
@@ -153,3 +155,137 @@ def handleCourseSearch(university: str, args: dict) -> list:
     results = query.all()
 
     return results
+
+
+class CourseSearch:
+    QUERY_DELIMITER = ","
+
+    # these constants help clean the query from malicious requests
+    VALID_PARAMETERS = [
+        "code",
+        "name",
+        "title",
+        "department",
+        "location",
+        "building",
+        "room",
+        "status",
+        "units",
+        "enrolled",
+        "waitlisted",
+        "requested",
+        "instructor"
+    ]
+
+    VALID_FILTERS = [
+        "like",
+        "equals",
+        "not",
+        "notlike"
+    ]
+
+    def __init__(self, university: str, args: dict):
+
+        self.args = args
+        self.query = University.query.filter_by(name = university).first().courses
+
+
+
+    def checkToInt(self, column: str, value: str):
+        '''
+        tbh this feels like a terrible function
+        but im gonna do it
+
+        returns string when the column is string
+        returns int when the column is int
+        '''
+        intColumns = ["units", "enrolled",
+                    "requested", "waitlisted"]
+
+        if (column in intColumns):
+            return int(value)
+        return value
+
+
+
+    def _addLikeFilter(self, column: str, filter: str, values: list):
+        '''
+        Adds LIKE filter to query
+        '''
+        for value in values:
+            value = value.upper()
+            self.query = self.query.filter(Courses.__table__.c[column].ilike(f"%{value}%"))
+
+
+
+    def _addNotLikeFilter(self, column: str, filter: str, values: list):
+        '''
+        Adds LIKE filter to query
+        '''
+        for value in values:
+            value = value.upper()
+            self.query = self.query.filter(~Courses.__table__.c[column].ilike(f"%{value}%"))
+
+
+
+    def _addEqualsFilter(self, column: str, filter: str, values: list):
+        '''
+        Adds EQUALS filter to query
+        '''
+        self.query = self.query.filter(Courses.__table__.c[column].in_(values))
+
+
+
+    def _addNotEqualsFilter(self, column: str, filter: str, values: list):
+        '''
+        Adds NOT EQUALS filter to query
+        '''
+        self.query = self.query.filter(~Courses.__table__.c[column].in_(values))
+
+
+
+    def _buildQuery(self):
+        # within args, a query is defined as:
+        # {"parameter[filter]": "value1,value2,etc"}
+        # We parse over the keys and values of args to build the query
+        for arg in self.args:
+            # parameter is defined before filter
+            parameter = arg.split("[")[0].lower()
+
+            queryValues = [self.checkToInt(parameter, v.upper()) for v in self.args[arg].split(self.QUERY_DELIMITER)]
+
+            # filter is defined between brackets; [filter]
+            filter = ""
+
+            try:
+                filter = re.search(r"\[([A-Za-z0-9_]+)\]", arg).group(1).lower()
+            except AttributeError:
+                # catch error when no filter is specified
+                # when no filter is specified, filter is assumed to be equals; "="
+                pass
+
+
+            # clean query
+            if (parameter in self.VALID_PARAMETERS):
+
+                # add filters
+                print(f"filter = {filter}")
+                if (filter == "like"):
+                    self._addLikeFilter(parameter, filter, queryValues)
+
+                elif (filter == "notlike"):
+                    self._addNotLikeFilter(parameter, filter, queryValues)
+
+                elif (filter == "not"):
+                    self._addNotEqualsFilter(parameter, filter, queryValues)
+
+                elif (filter == "equals" or filter == ""):
+                    self._addEqualsFilter(parameter, filter, queryValues)
+
+
+
+    def search(self):
+        self._buildQuery()
+        results = self.query.all()
+
+        return results
