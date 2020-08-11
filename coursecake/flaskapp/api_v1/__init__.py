bf722b60ece5c85538e7b7c71dc779f5979d6265
nananananate <piewalker442@gmail.com>
2020-08-11 14:33:14 -0700

    changed doc path to prevent bugs

--- coursecake/flaskapp/api_v1/__init__.py
+++ coursecake/flaskapp/api_v1/__init__.py
@@ -10,7 +10,6 @@ blueprint = Blueprint("api_v1", __name__, url_prefix="/api/v1")
 
 api = Api(
     blueprint,
-    doc="/doc/",
     title="CourseCake",
     version="1.0",
     description="Reworked API using Flask-RestX"
