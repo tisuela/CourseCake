e4a7cc62b177ace7ad22eee5eea3779515d96b5d
nananananate <piewalker442@gmail.com>
2020-08-22 22:49:09 -0700

    added auth

--- coursecake/fastapi_app/api_v1/admin/routes.py
+++ coursecake/fastapi_app/api_v1/admin/routes.py
@@ -1,11 +1,13 @@
 # contains all routes for the courses endpoint
 from typing import List
 
-from fastapi import Depends, APIRouter, BackgroundTasks
+from fastapi import Depends, APIRouter, BackgroundTasks, status, HTTPException
 from sqlalchemy.orm import Session
 
 from ....database import crud, models, sql, uploads
 from .. import schemas
+from . import utils
+
 router = APIRouter()
 
 # dependency
@@ -21,7 +23,14 @@ def get_db():
 
 
 @router.post("/update/all")
-async def update_all(background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+async def update_all(token: str, background_tasks: BackgroundTasks, db: Session = Depends(get_db)):
+
+    if not utils.verifyAdminToken(token):
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect token"
+        )
+
     background_tasks.add_task(uploads.update_all, db)
 
     return {"message": "initiated updates for all database information via scrapers. this will take a few minutes."}
