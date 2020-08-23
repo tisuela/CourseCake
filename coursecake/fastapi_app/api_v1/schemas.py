fbca1e4bd35b095228644e8c0f51ba337823d4dd
nananananate <piewalker442@gmail.com>
2020-08-22 19:50:48 -0700

    created base schemas

---
+++ coursecake/fastapi_app/api_v1/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+
+class UniversityBase(BaseModel):
+    name: str
+
+
+class UniversityCreate(UniversityBase):
+    pass
+
+
+class University(UniversityBase):
+
+    class Config:
+        orm_mode = True
+
+class CourseBase(BaseModel):
+    university_name: str
+    term_id: str
+    code: str
+
+
+class CourseCreate(CourseBase):
+    pass
+
+
+class Course(CourseBase):
+
+    class Config:
+        orm_mode = True
